c162e5436ceb08299f844a905aef75417b0f7196
Sam Guliker <samguliker@Sams-MacBook-Air.local>
2021-11-09 08:14:48 +0100

    trying to fix the doubleclick problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect} from 'react';
+import { motion } from "framer-motion"
 import axios from 'axios'
 
 import './App.css';
@@ -9,28 +10,28 @@ import './App.css';
   Een shuffle card game maken.
 
   Functies:
-  1. API call - connect to deck 👌
+  1. API Call - Connect to deck 👌
   2. Render it's propperties 👌
   3. Get random card from the same deck. 👌
   4. Shuffle knop. 👌
   5. Tap and get a random card. 👌
 
   Optional:
-  5. List of the previous cards.
-  6. Shuffle on gesture card press
+  5. List of the previous cards
+  6. Shuffle on gesture card press 
 
   Technologies: 
   - Typescript 👌
   - Use Axios 👌
     https://github.com/axios/axios
 
-  Update *
+  Update 
   1. Zorg ervoor dat er een state is dat bijhoudt welke deck je gebruikt.
   2. Wanneer er een random kaart gepakt wordt moet het van dezelfde deck komen.
 
   Game states:
   - Start, draw a card! 👌
-  - Out of cards , restart. 👌
+  - Out of cards, restart. 👌
 
 */
 
@@ -39,6 +40,10 @@ function App() {
   const [deckDataID, setDeckDataID] = useState<any>(null)
   const [randomCard, setRandomCard] = useState<any>(null)
   const [remainingCard, setRemainingCard] = useState<number>(52)
+
+  // const mobileWidth:number = 768
+  // const width  = window.innerWidth || document.documentElement.clientWidth || 
+  // document.body.clientWidth;
 
   useEffect(() => {
      async function getCardsData() {
@@ -52,6 +57,12 @@ function App() {
     } 
     getCardsData()
   }, [])
+
+  const handleOnclick = (e:any) => {
+    e.preventDefault()
+    console.log(e)
+    getRandomCard()
+  }
 
   const getRandomCard = () => {
     async function getRandomCardFetch() {
@@ -93,16 +104,16 @@ function App() {
     <div className="App">
       <div className="card-container">
         {randomCard === null ? (
-            <button onClick={() => {getRandomCard()}}> Press to start!</button>
+            <button onClick={(e) => { handleOnclick(e) }}> Press to start!</button>
           )
           :
           (
-            <img 
+            <motion.img 
               onClick={() => {getRandomCard()}}
               src={randomCard.image}
               alt={randomCard.image}
-              >
-            </img>
+              whileTap={{ scale: 0.9 }}
+            />
           )
 
         }
